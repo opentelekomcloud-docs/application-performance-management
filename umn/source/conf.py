e5dc0721a3ce31b3f319598bdfb4ee7b83adc5bd
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2024-12-10 15:12:21 +0100

    Update tox.ini && conf.py file (#18)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Tino Schr

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -110,7 +110,7 @@ html_copy_source = False
 latex_documents = [
     ('index',
      'apm-umn.tex',
-     u'Application Performance Management - User Guide',
+    u'Application Performance Management - User Guide',
      u'OpenTelekomCloud', 'manual'),
 ]
 
